3a72a76351bc9b1e43dc407d386f4e65fbed3c08
Pawel <pepawel@users.noreply.github.com>
2023-09-23 20:02:48 +0200

    Make basic loader standard ruby compliant

--- lib/basic_loader.rb
+++ lib/basic_loader.rb
@@ -2,3 +2,10 @@
 
 # File generated automatically, do not edit
 
+require "boot_parser/config"
+require "boot_parser/parser"
+require "boot_parser/driver/grub"
+require "boot_parser/entry/generic"
+require "boot_parser/entry/linux"
+require "boot_parser/entry/unsupported"
+require "boot_parser/driver/grub/lexer"
