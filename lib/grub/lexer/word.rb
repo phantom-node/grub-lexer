3d985ae091b18d66029e25457135970ea13aa05d
Pawel <pepawel@users.noreply.github.com>
2023-10-20 18:42:04 +0200

    Adjust Word class namespace

--- lib/grub/lexer/word.rb
+++ lib/grub/lexer/word.rb
@@ -2,31 +2,29 @@
 
 module Grub
   class Lexer
-    class Lex
-      class Word
-        attr_reader :parts
+    class Word
+      attr_reader :parts
 
-        def append(text)
-          parts << text.to_s
-        end
+      def append(text)
+        parts << text.to_s
+      end
 
-        def variable(v)
-          parts << v.to_sym
-        end
+      def variable(v)
+        parts << v.to_sym
+      end
 
-        def present?
-          !blank?
-        end
+      def present?
+        !blank?
+      end
 
-        def blank?
-          parts.empty?
-        end
+      def blank?
+        parts.empty?
+      end
 
-        private
+      private
 
-        def initialize
-          @parts = []
-        end
+      def initialize
+        @parts = []
       end
     end
   end
